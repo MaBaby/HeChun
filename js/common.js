58dc89d79908da61b2f970ae7c301648290b46e1
beibei <2360608867@qq.com>
2016-06-28 18:42:17 +0800

    .

---
+++ js/common.js
@@ -0,0 +1,22 @@
+//var music = document.getElementById("music");
+//var audio = document.getElementsByTagName("audio")[0];
+//music.onclick = function(){
+//	audio.pause();
+//}
+
+window.onload = function(){
+	var music = document.getElementById("music");
+	var audio = document.getElementsByTagName("audio")[0];
+	
+	music.onclick = function(){
+		if(audio.paused){
+			audio.play();
+//			this.setAttribute("class","play");
+            this.style.animationPlayState = "running";
+		}else{
+			audio.pause();	
+//			this.setAttribute("class","");
+            this.style.animationPlayState = "paused";
+		}
+	}
+}
