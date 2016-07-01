141c47c14fd355be163d1973bc699bdc0ea41acc
beibei <2360608867@qq.com>
2016-07-01 18:40:42 +0800

    .

--- js/common.js
+++ js/common.js
@@ -1,36 +1,57 @@
 
 window.onload = function(){
+    //获取元素
+    var page1 = document.getElementById("page1");
+    var page2 = document.getElementById("page2");
+    var page3 = document.getElementById("page3");
+
 	var music = document.getElementById("music");
 	var audio = document.getElementsByTagName("audio")[0];
 
     //当音乐播放完停止时候，自动停止光盘旋转效果
 	audio.addEventListener("ended" ,function(event){
 		music.setAttribute("class","");
-        //this.style.animationPlayState = "paused";
-        //this.style.webkitAnimationPlayState = "paused";
+        this.style.animationPlayState = "paused";
+        this.style.webkitAnimationPlayState = "paused";
 
 	},false);
 	//点击音乐图标，控制音乐播放效果
-	//music.onclick = function (){
-	//	if(audio.paused){
-	//		audio.play();
-	//		//this.setAttribute("class","play");    //停止时图片回到原始的地方
-     //       this.style.animationPlayState = "running";   //兼容不好，安卓4.4以上版本，iOS不能用
-	//		this.style.webkitAnimationPlayState = "running";
-	//	}else{
-	//		audio.pause();
-	//		//this.setAttribute("class","");
-     //       this.style.animationPlayState = "paused";
-	//		this.style.webkitAnimationPlayState = "paused";
-	//	}
-	//}
+	music.onclick = function (){
+		if(audio.paused){
+			audio.play();
+			//this.setAttribute("class","play");    //停止时图片回到原始的地方
+            this.style.animationPlayState = "running";   //兼容不好，安卓4.4以上版本，iOS不能用
+			this.style.webkitAnimationPlayState = "running";
+		}else{
+			audio.pause();
+			//this.setAttribute("class","");
+            this.style.animationPlayState = "paused";
+			this.style.webkitAnimationPlayState = "paused";
+		}
+	}
     music.addEventListener("touthstart",function(event){
         if(audio.paused){
             audio.play();
             this.setAttribute("class","play");
+            this.style.animationPlayState = "running";   //兼容不好，安卓4.4以上版本，iOS不能用
+            this.style.webkitAnimationPlayState = "running";
         }else {
             audio.pause();
             this.setAttribute("class","");
+            this.style.animationPlayState = "paused";
+            this.style.webkitAnimationPlayState = "paused";
         };
+    },false);
+    page1.addEventListener("touchstart",function(event){
+        page1.style.display = "none";
+        page2.style.display = "block";
+        page3.style.display = "block";
+        page3.style.top     = "100%";
+
+
+        setTimeout(function(){
+            page2.setAttribute("class","page fadeOut");
+            page3.setAttribute("class","page fadeIn");
+        },5500);
     },false);
 };
